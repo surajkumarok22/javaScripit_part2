b98070a9852bf3008f5121ba76d8fa5c4b764adf
Suraj Kumar <surajkumarok20@gmail.com>
2024-10-16 14:34:05 +0530

    project second

---
+++ project2/app.js
@@ -0,0 +1,25 @@
+const form = document.querySelector('form');
+// this usecase will give you empty value
+// const height =   parseInt(document.querySelector('#height').values)
+
+console.log(form);
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const height = parseInt(document.querySelector('#height').value)
+
+    const weight = parseInt(document.querySelector('#weight').value)
+
+    const results = document.querySelector('#results')
+
+    if (height === '' || height < 0 || isNaN(height)) {
+        results.innerHTML = `please give a valid height ${height}`;
+      } else if (weight === '' || weight < 0 || isNaN(weight)) {
+        results.innerHTML = `please give a valid weight ${weight}`;
+      } else {
+        const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+        // show the results
+        results.innerHTML = `<span>${bmi}</span>`;
+      }
+
+})
